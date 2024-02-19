1cd152afdd7a8331b8f1c0370c37a03952a76157
yasasvi <yasasvigumma@gmail.com>
2024-02-19 22:53:38 +0530

    created a small project with bun

--- index.ts
+++ index.ts
@@ -1,23 +1,20 @@
 import { serve } from "bun";
 
-function App({ path }) {
-  // Define your components and JSX here
-  return (
-    <html>
-      <body>
-        <h1>Dynamic Path: {path}</h1>
-      </body>
-    </html>
-  );
+function App(path: string) {
+  return /*html*/ ` 
+  <html>
+    <body>
+      <h1>Dynamic Path: ${path}</h1>
+    </body>
+  </html>`;
 }
-
 serve({
   fetch(req) {
     // Extract the path from the request URL
     const { pathname } = new URL(req.url);
 
     // Convert the JSX component to an HTML string
-    const html = Bun.renderToString(<App path={pathname} />);
+    const html = App(pathname);
 
     // Return the HTML content as a response
     return new Response(html, {
@@ -26,4 +23,5 @@ serve({
       },
     });
   },
+  port: 5000,
 });
